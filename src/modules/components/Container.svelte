9617200afdf668b1326e2d1338bbe644d45df8a5
typhonrt <support@typhonjs.io>
2021-11-03 06:04:06 -0700

    Add container warning prop to Container.

--- src/modules/components/Container.svelte
+++ src/modules/components/Container.svelte
@@ -1,12 +1,14 @@
 <script>
+   export let warn = false;
    export let children;
 </script>
+
 
 {#if Array.isArray(children)}
     {#each children as child}
         <svelte:component this={child.class} {...child.props} />
     {/each}
-{:else}
+{:else if warn}
     <p>Container warning: No children.</p>
 {/if}
 
