3858c28de62162f3d3f0b06e30cb7f6c6d6dcca4
typhonrt <support@typhonjs.io>
2021-11-07 02:55:38 -0800

    Fixed circular references

--- src/modules/index-svelte.js
+++ src/modules/index-svelte.js
@@ -1,9 +1,3 @@
 // Provides all exports for Svelte to examine; used in `package.json`.
 export * from './application';
 export * from './component';
-export * from './gsap';
-export * from './handler';
-export * from './helper';
-export * from './store';
-export * from './transition';
-export * from './util';
