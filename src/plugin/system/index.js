9335a2a39c27660fe8665fe2dede299ff5e5ce3a
typhonrt <support@typhonjs.io>
2021-12-24 14:55:07 -0800

    Renamed SettingsControl to TJSSettingsControl

--- src/plugin/system/index.js
+++ src/plugin/system/index.js
@@ -1,4 +1,4 @@
 export * from '@typhonjs-svelte/lib/plugin/system';
 
-export * from './settings/SettingsControl.js';
+export * from './settings/TJSSettingsControl.js';
 export * from './settings/store/TJSGameSettings.js';
