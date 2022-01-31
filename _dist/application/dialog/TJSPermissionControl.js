f848c6024f0dbc044ffd84ba0ad3f5ca7c9c58e4
typhonrt <support@typhonjs.io>
2022-01-31 12:52:10 -0800

    massive improvements. TJSDialog can now get mounted Svelte component, TJSPermissionControl added, stores for documents and collections.

---
+++ _dist/application/dialog/TJSPermissionControl.js
@@ -0,0 +1,37 @@
+import { TJSPermissionControl
+    as TJSPermissionControlImpl }   from '@typhonjs-fvtt/svelte/component/core';
+
+import { hasSetter }                from '@typhonjs-fvtt/svelte/util';
+
+import { TJSDialog }                from './TJSDialog.js';
+
+/**
+ * Provides a reactive dialog for permission control that by default is modal and not draggable. An additional set of
+ * accessors for the document assigned are available via the `this.reactive.document`. You may swap out the document at
+ * any time by setting it to a different document.
+ */
+export class TJSPermissionControl extends TJSDialog
+{
+   constructor(document, data = {})
+   {
+      super({
+         modal: true,
+         draggable: false,
+         ...data,
+         content: {
+            class: TJSPermissionControlImpl,
+            props: { document }
+         },
+         title: `${game.i18n.localize('PERMISSION.Title')}: ${document.name}`
+      });
+
+      // Add accessors to reactive data for 'document'.
+      Object.defineProperty(this.reactive, 'document', {
+         get: () => this?.dialogComponent?.document,
+         set: (document) =>
+         {
+            if (hasSetter(this?.dialogComponent, 'document')) { this.dialogComponent.document = document; }
+         }
+      });
+   }
+}
