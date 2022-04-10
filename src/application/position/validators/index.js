ab797ae6cb849d3c21960a914cd9e9b5b9173627
typhonrt <support@typhonjs.io>
2022-04-09 19:19:47 -0700

    Completed unified position validators.

--- src/application/position/validators/index.js
+++ src/application/position/validators/index.js
@@ -1,3 +1,7 @@
-export * from './basicWindow.js';
-export * from './InsideElement.js';
-export * from './transformWindow.js';
+import { BasicBounds }     from './BasicBounds.js';
+import { TransformBounds } from './TransformBounds.js';
+
+const basicWindow = new BasicBounds({ lock: true });
+const transformWindow = new TransformBounds({ lock: true });
+
+export { basicWindow, BasicBounds, transformWindow, TransformBounds };
