d3c4b017df079575a0297a7efbc07d666cdffce5
typhonrt <support@typhonjs.io>
2022-05-18 22:22:36 -0700

    Added initial ease implementation.

--- _dist/action/index.js
+++ _dist/action/index.js
@@ -1,6 +1,7 @@
 import 'svelte/store';
 import 'svelte/internal';
 import { hasSetter } from '@typhonjs-fvtt/svelte/util';
+import { cubicOut } from 'svelte/easing';
 
 /**
  * Provides a basic test for a given variable to test if it has the shape of a writable store by having a `subscribe`
@@ -468,9 +469,14 @@ function applyPosition(node, position)
  *
  * @param {Writable<boolean>} [params.storeDragging] - A writable store that tracks "dragging" state.
  *
+ * @param {boolean}           [params.ease=true] - When true easing is enabled.
+ *
+ * @param {object}            [params.easeOptions] - Gsap `to / `quickTo` vars object.
+ *
  * @returns {{update: Function, destroy: Function}} The action lifecycle methods.
  */
-function draggable(node, { position, active = true, storeDragging = void 0 })
+function draggable(node, { position, active = true, storeDragging = void 0, ease = false,
+ easeOptions = { duration: 0.1, ease: cubicOut } })
 {
    /**
     * Duplicate the app / Positionable starting position to track differences.
@@ -493,6 +499,13 @@ function draggable(node, { position, active = true, storeDragging = void 0 })
     * @type {boolean}
     */
    let dragging = false;
+
+   /**
+    * Stores the ease tween.
+    *
+    * @type {TJSBasicAnimation}
+    */
+   let tweenTo;
 
    /**
     * Remember event handlers associated with this action so they may be later unregistered.
@@ -572,11 +585,24 @@ function draggable(node, { position, active = true, storeDragging = void 0 })
          storeDragging.set(true);
       }
 
-      s_POSITION_DATA.left = initialPosition.left + (event.clientX - initialDragPoint.x);
-      s_POSITION_DATA.top = initialPosition.top + (event.clientY - initialDragPoint.y);
+      /** @type {number} */
+      const newLeft = initialPosition.left + (event.clientX - initialDragPoint.x);
+      /** @type {number} */
+      const newTop = initialPosition.top + (event.clientY - initialDragPoint.y);
 
-      // Update application position.
-      position.set(s_POSITION_DATA);
+      if (ease)
+      {
+         if (tweenTo) { tweenTo.cancel(); }
+
+         tweenTo = position.animateTo({ left: newLeft, top: newTop }, easeOptions);
+      }
+      else
+      {
+         s_POSITION_DATA.left = newLeft;
+         s_POSITION_DATA.top = newTop;
+
+         position.set(s_POSITION_DATA);
+      }
    }
 
    /**
@@ -605,6 +631,13 @@ function draggable(node, { position, active = true, storeDragging = void 0 })
             if (active) { activateListeners(); }
             else { removeListeners(); }
          }
+
+         if (typeof options.ease === 'boolean') { ease = options.ease; }
+
+         if (typeof options.easeOptions === 'object')
+         {
+            easeOptions = options.easeOptions;
+         }
       },
 
       destroy: () => removeListeners()
@@ -613,6 +646,10 @@ function draggable(node, { position, active = true, storeDragging = void 0 })
 
 class DraggableOptions
 {
+   #ease = false;
+
+   #easeOptions = { duration: 0.1, ease: cubicOut };
+
    /**
     * Stores the subscribers.
     *
@@ -622,6 +659,86 @@ class DraggableOptions
 
    constructor()
    {
+      // Define the following getters directly on this instance and make them enumerable. This allows them to be
+      // picked up w/ `Object.assign`.
+      Object.defineProperty(this, 'ease', {
+         get: () => { return this.#ease; },
+         set: (ease) =>
+         {
+            if (typeof ease !== 'boolean') { throw new TypeError(`'ease' is not a boolean.`); }
+
+            this.#ease = ease;
+            this.#updateSubscribers();
+         },
+         enumerable: true
+      });
+
+      Object.defineProperty(this, 'easeOptions', {
+         get: () => { return this.#easeOptions; },
+         set: (easeOptions) => { this.#easeOptions = easeOptions; this.#updateSubscribers(); },
+         enumerable: true
+      });
+   }
+
+
+   /**
+    * @returns {number} Get ease duration
+    */
+   get easeDuration() { return this.#easeOptions.duration; }
+
+   /**
+    * @returns {string|Function} Get easing function value.
+    */
+   get easeValue() { return this.#easeOptions.ease; }
+
+
+   /**
+    * @param {number}   duration - Set ease duration.
+    */
+   set easeDuration(duration)
+   {
+      if (!Number.isFinite(duration))
+      {
+         throw new TypeError(`'duration' is not a finite number.`);
+      }
+
+      if (duration < 0) { throw new Error(`'duration' is less than 0.`); }
+
+      this.#easeOptions.duration = duration;
+      this.#updateSubscribers();
+   }
+
+   /**
+    * @param {string|Function} value - Get easing function value.
+    */
+   set easeValue(value)
+   {
+      if (typeof value !== 'function' && typeof value !== 'string')
+      {
+         throw new TypeError(`'value' is not a function or string.`);
+      }
+
+      this.#easeOptions.ease = value;
+      this.#updateSubscribers();
+   }
+
+   /**
+    * Resets all options data to default values.
+    */
+   reset()
+   {
+      this.#ease = false;
+      this.#easeOptions = { duration: 0.1, ease: cubicOut };
+      this.#updateSubscribers();
+   }
+
+   /**
+    * Resets easing options to default values.
+    */
+   resetEase()
+   {
+      this.#easeOptions = { duration: 0.1, ease: cubicOut };
+      this.#updateSubscribers();
    }
 
    /**
