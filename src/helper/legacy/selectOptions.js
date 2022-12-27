7d5172940e779480a773df86922e5e3ec4e5e3ca
typhonrt <support@typhonjs.io>
2022-12-26 22:40:14 -0800

    Updated legacy helpers.

--- src/helper/legacy/selectOptions.js
+++ src/helper/legacy/selectOptions.js
@@ -1,7 +1,7 @@
 /**
  * Converts the `selectOptions` Handlebars helper to be Svelte compatible. This is useful when initially converting
  * over an app to Svelte and for essential usage to several dialogs that mirror the core Foundry experience. For
- * an example of usage see {@link TJSFolderPermissions}.
+ * an example of usage see {@link TJSDocumentOwnership}.
  *
  * A helper to create a set of <option> elements in a <select> block based on a provided dictionary.
  * The provided keys are the option values while the provided values are human-readable labels.
@@ -75,9 +75,6 @@ export function selectOptions(choices, options)
 
    selected = selected instanceof Array ? selected.map(String) : [String(selected)];
 
-console.log(`!! selectOptions - 0 - choices: `, choices);
-console.log(`!! selectOptions - 1 - options: `, options);
-
    // Prepare the choices as an array of objects
    const selectChoices = [];
 
@@ -113,63 +110,14 @@ console.log(`!! selectOptions - 1 - options: `, options);
       selectChoices.unshift({ name: '', label });
    }
 
-console.log(`!! selectOptions - 2 - selectChoices: `, selectChoices);
-
    // Create the HTML
    let html = '';
    for (const option of selectChoices)
    {
-console.log(`!! selectOptions - 3 - option: `, option);
       const label = globalThis.Handlebars.escapeExpression(option.label);
       const isSelected = selected.includes(option.name);
       html += `<option value="${option.name}" ${isSelected ? "selected" : ""}>${label}</option>`;
    }
 
-console.log(`!! selectOptions - 4 - html: `, html);
-
-console.log(`!! selectOptions - 5 - globalThis.Handlebars.SafeString(html): `, globalThis.Handlebars.SafeString(html));
-
    return new globalThis.Handlebars.SafeString(html);
-
-   // const localize = options['localize'] ?? false;
-   // let selected = options['selected'] ?? null;
-   // const blank = options['blank'] ?? null;
-   // const nameAttr = options['nameAttr'];
-   // const labelAttr = options['labelAttr'];
-   // const inverted = !!options['inverted'];
-   //
-   // selected = selected instanceof Array ? selected.map(String) : [String(selected)];
-   //
-   // // Create an option
-   // const option = (name, label) =>
-   // {
-   //    if (localize) { label = game.i18n.localize(label); }
-   //    const isSelected = selected.includes(String(name));
-   //
-   //    html += `<option value="${name}" ${isSelected ? "selected" : ""}>${label}</option>`;
-   // };
-   //
-   // // Create the options
-   // let html = '';
-   // if (blank !== null) { option('', blank); }
-   //
-   // // Options as an Array
-   // if (choices instanceof Array)
-   // {
-   //    for (const choice of choices) { option(choice[nameAttr], choice[labelAttr]); }
-   // }
-   //
-   // // Choices as an Object
-   // else
-   // {
-   //    for (const choice of Object.entries(choices))
-   //    {
-   //       let [key, value] = inverted ? choice.reverse() : choice;
-   //       if (nameAttr) { key = value[nameAttr]; }
-   //       if (labelAttr) { value = value[labelAttr]; }
-   //       option(key, value);
-   //    }
-   // }
-   //
-   // return html;
 }
