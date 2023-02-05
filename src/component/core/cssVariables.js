49ca8f351a7c81432b65828affe51000f5b8e8d4
typhonrt <support@typhonjs.io>
2023-02-04 16:14:26 -0800

    Added version to cssVariables.

--- src/component/core/cssVariables.js
+++ src/component/core/cssVariables.js
@@ -2,6 +2,6 @@ import { StyleManager } from '@typhonjs-fvtt/svelte/util';
 
 const s_STYLE_KEY = '#__trl-root-styles';
 
-const cssVariables = new StyleManager({ docKey: s_STYLE_KEY });
+const cssVariables = new StyleManager({ docKey: s_STYLE_KEY, version: 1 });
 
 export { cssVariables };
