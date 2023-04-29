b5402c9f7ef77e7b2bd1bc5b3f61d454f5b61192
typhonrt <support@typhonjs.io>
2023-04-28 17:28:30 -0700

    Update types.

--- src/store/gameState.js
+++ src/store/gameState.js
@@ -1,27 +1,18 @@
 import { writable } from 'svelte/store';
 
-const storeState = writable(void 0);
+const _storeGameState = writable(void 0);
 
 /**
- * @type {GameState} Provides a Svelte store wrapping the Foundry runtime / global game state.
+ * @type {import('svelte/store').Readable<globalThis.game>} Provides a Svelte store wrapping the Foundry `game` global
+ * variable. It is initialized on the `ready` hook. You may use this store to access the global game state from a
+ * Svelte template. It is a read only store and will receive no reactive updates during runtime.
  */
 const gameState = {
-   subscribe: storeState.subscribe,
-   get: () => game
+   subscribe: _storeGameState.subscribe,
 };
 
 Object.freeze(gameState);
 
-Hooks.once('ready', () => storeState.set(game));
+Hooks.once('ready', () => _storeGameState.set(game));
 
 export { gameState };
-
-/**
- * @typedef {import('svelte/store').Readable} GameState - Provides a Svelte store wrapping the Foundry `game` global variable. It is initialized
- * on the `ready` hook. You may use this store to access the global game state from a Svelte template. It is a read only
- * store and will receive no reactive updates during runtime.
- *
- * @property {import('svelte/store').Readable.subscribe} subscribe - Provides the Svelte store subscribe function.
- *
- * @property {Function} get - Provides a mechanism to directly access the Foundry game state without subscribing.
- */
