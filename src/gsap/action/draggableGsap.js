5d12c586973b2c12537b6dc3c601e25d392221fc
typhonrt <support@typhonjs.io>
2023-07-01 18:29:52 -0700

    eslint fixes

--- src/gsap/action/draggableGsap.js
+++ src/gsap/action/draggableGsap.js
@@ -12,8 +12,9 @@ const s_HAS_QUICK_TO = false;
 
 /**
  * Provides an action to enable pointer dragging of an HTMLElement using GSAP `to` or `quickTo` to invoke `position.set`
- * on a given {@link TJSPosition} instance provided. You may provide a `easeOptions` object sent to the tween to modify
- * the duration / easing. When the attached boolean store state changes the draggable action is enabled or disabled.
+ * on a given {@link import('#runtime/svelte/store/position').TJSPosition} instance provided. You may provide a
+ * `easeOptions` object sent to the tween to modify the duration / easing. When the attached boolean store state
+ * changes the draggable action is enabled or disabled.
  *
  * Note: Requires GSAP `3.10+` for `quickTo` support.
  *
