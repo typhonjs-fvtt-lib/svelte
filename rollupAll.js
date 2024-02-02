1b5d523b26c9a68acb2cefc75ad00de97ceb226d
typhonrt <support@typhonjs.io>
2024-02-01 16:41:05 -0800

    Update configuration.

--- rollupAll.js
+++ rollupAll.js
@@ -287,3 +287,15 @@ await generateDTS({
    output: './_dist/application/legacy/index.d.ts',
    ...applicationDTSOptions
 });
+
+console.log('Generating TS Declaration: ./_dist/component/core/index.js');
+
+await generateDTS({
+   input: './_dist/component/core/index.js'
+});
+
+console.log('Generating TS Declaration: ./_dist/component/internal/index.js');
+
+await generateDTS({
+   input: './_dist/component/internal/index.js', logLevel: 'debug'
+});
