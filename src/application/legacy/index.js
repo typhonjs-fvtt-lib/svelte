59e224929da20cd5b19e40b3bf6f5a12bbdb6acf
typhonrt <support@typhonjs.io>
2024-09-30 16:26:11 -0700

    Add module description.

--- src/application/legacy/index.js
+++ src/application/legacy/index.js
@@ -1,3 +1,11 @@
+/**
+ * Provides legacy support for Handlebars rendering while still gaining aspects of reactivity with a Svelte powered
+ * application shell. You may use the resources in this package in a similar manner as the core Foundry
+ * {@link Application} / {@link FormApplication} classes. This should only be an interim or stepwise solution as you
+ * convert your package over to fully using TRL & Svelte.
+ *
+ * @module
+ */
 import { HandlebarsApplication }       from './HandlebarsApplication.js';
 import { HandlebarsFormApplication }   from './HandlebarsFormApplication.js';
 import { SvelteFormApplication }       from './SvelteFormApplication.js';
