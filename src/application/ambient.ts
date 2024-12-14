37ad405b2cb5304c8a331f88fb1dc9499aae0075
typhonrt <support@typhonjs.io>
2024-12-14 06:21:37 -0800

    Update types.

--- src/application/ambient.ts
+++ src/application/ambient.ts
@@ -6,7 +6,7 @@ declare global {
 
 // @ts-ignore This is bundled and is not an error.
 declare module '@typhonjs-fvtt/svelte/application' {
-   export interface SvelteApplication<Options extends SvelteApp.Options> extends Application<Options>
+   export interface SvelteApplication<Options extends SvelteApp.Options = SvelteApp.Options> extends Application<Options>
    {
       options: Options;
    }
