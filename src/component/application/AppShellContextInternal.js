e0fed56eeb965c7e71796cc6a58f52a81490be2c
typhonrt <support@typhonjs.io>
2025-09-18 01:42:57 -0700

    Add additional `#internal` stores for CQ & content constraints.

--- src/component/application/AppShellContextInternal.js
+++ src/component/application/AppShellContextInternal.js
@@ -9,12 +9,20 @@ import { writable } from '#svelte/store';
  */
 export class AppShellContextInternal
 {
-   /** @type {import('./types').AppShell.Context.Internal.stores} */
+   /** @type {import('./types').AppShell.Context.InternalAppStores} */
    #stores;
 
    constructor()
    {
       this.#stores = {
+         // When app shell has content resize observation enabled these stores are updated.
+         contentOffsetWidth: writable(0),
+         contentOffsetHeight: writable(0),
+         contentWidth: writable(0),
+         contentHeight: writable(0),
+
+         cqEnabled: writable(false),
+
          elementContent: writable(void 0),
          elementRoot: writable(void 0)
       };
@@ -24,8 +32,10 @@ export class AppShellContextInternal
    }
 
    /**
-    * @returns {import('./types').AppShell.Context.Internal.stores} The internal context stores for `elementContent` /
-    *          `elementRoot`
+    * @returns {import('./types').AppShell.Context.InternalAppStores} The internal context stores:
+    * - `cqEnabled` - Container query enabled state.
+    * - `elementContent` - The bound elementContent element reference.
+    * - `elementRoot` - The bound elementRoot element reference.
     */
    get stores()
    {
