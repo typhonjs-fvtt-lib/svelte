2579f4412f55949a9f2b422e6ffbf91d70257772
typhonrt <support@typhonjs.io>
2025-10-25 13:34:47 -0700

    Refactored WorldSettingArrayStore to GameSettingArrayObject supporting both `world` and `user` scope configuration.

--- src/store/fvtt/settings/array-object/GameSettingArrayObject.ts
+++ src/store/fvtt/settings/array-object/GameSettingArrayObject.ts
@@ -1,50 +1,63 @@
 import { CrudArrayObjectStore }  from '#runtime/svelte/store/reducer/array-object';
 import { Hashing }               from '#runtime/util';
+import { isObject }              from '#runtime/util/object';
 
 import type { TJSGameSettings }  from '#svelte-fvtt/store/fvtt/settings';
 
 /**
  * Defines a {@link CrudArrayObjectStore} with streamlined configuration through {@link TJSGameSettings} to register
- * a world game setting. WorldArrayObjectStore is automatically associated as the store receiving updates with the
- * associated game setting.
+ * a `world` or `user` game setting. `GameSettingArrayObject` is automatically associated as the store receiving
+ * updates with the associated game setting.
  *
  * @typeParam S - Store type.
  */
-class WorldArrayObjectStore<S extends WorldArrayObjectStore.Data.BaseObjectEntryStore<any>> extends
+class GameSettingArrayObject<S extends GameSettingArrayObject.Data.BaseObjectEntryStore<any>> extends
  CrudArrayObjectStore<S>
 {
    /**
-    * Game setting 'key' field.
+    * Game setting `key` field.
     */
    readonly #key: string;
 
    /**
-    * Game setting 'namespace' field.
+    * Game setting `namespace` field.
     */
    readonly #namespace: string;
 
    /**
-    * @param options - WorldArrayObjectStore Options.
+    * Game setting `scope` field.
     */
-   constructor({ namespace, key, defaultData = [], gameSettings, ...rest }: WorldArrayObjectStore.Options.Config<S>)
+   readonly #scope: string;
+
+   /**
+    * @param options - GameSettingArrayObject Options.
+    */
+   constructor({ namespace, key, scope, defaultData = [], gameSettings, ...rest }:
+    GameSettingArrayObject.Options.Config<S>)
    {
       super({
          ...rest,
-         extraData: { namespace, key }
+         extraData: { namespace, key, scope }
       });
 
       if (typeof key !== 'string') { throw new TypeError(`'key' is not a string.`); }
       if (typeof namespace !== 'string') { throw new TypeError(`'namespace' is not a string.`); }
+      if (typeof scope !== 'string') { throw new TypeError(`'scope' is not a string.`); }
+
+      if (scope !== 'world' && scope !=='user') { throw new Error(`'scope' must be 'world' or 'user'.`); }
 
       this.#namespace = namespace;
       this.#key = key;
+      this.#scope = scope;
 
       if (!Array.isArray(defaultData)) { throw new TypeError(`'defaultData' is not an array.`); }
 
       // Define default `id` if not provided in `defaultData` then create a random `UUIDv4` value.
       for (const entry of defaultData)
       {
-         if (typeof entry.id !== 'string') { entry.id = Hashing.uuidv4(); }
+         if (!isObject(entry)) { throw new TypeError(`'entry' in 'defaultData' is not an object.`); }
+
+         if (!Hashing.isUuidv4(entry.id)) { entry.id = Hashing.uuidv4(); }
       }
 
       if (gameSettings)
@@ -54,7 +67,7 @@ class WorldArrayObjectStore<S extends WorldArrayObjectStore.Data.BaseObjectEntry
             key,
             store: this,
             options: {
-               scope: 'world',
+               scope,
                config: false,
                default: Array.isArray(defaultData) ? defaultData : [],
                type: Array,
@@ -65,17 +78,22 @@ class WorldArrayObjectStore<S extends WorldArrayObjectStore.Data.BaseObjectEntry
    }
 
    /**
-    * @returns The Foundry game setting key.
+    * @returns The Foundry game setting `key`.
     */
    get key(): string { return this.#key; }
 
    /**
-    * @returns The Foundry game setting namespace.
+    * @returns The Foundry game setting `namespace`.
     */
    get namespace(): string { return this.#namespace; }
+
+   /**
+    * @returns The Foundry game setting `scope`.
+    */
+   get scope(): string { return this.#scope; }
 }
 
-declare namespace WorldArrayObjectStore {
+declare namespace GameSettingArrayObject {
    export import Data = CrudArrayObjectStore.Data;
    export import Util = CrudArrayObjectStore.Util;
 
@@ -85,28 +103,34 @@ declare namespace WorldArrayObjectStore {
        */
       export interface Config<S extends Data.BaseObjectEntryStore<any>> extends CrudArrayObjectStore.Options.Config<S> {
          /**
-          * Game setting 'namespace' field.
+          * Game setting `namespace` field.
           */
          namespace: string;
 
          /**
-          * Game setting 'key' field.
+          * Game setting `key` field.
           */
          key: string;
+
+         /**
+          * Game setting `scope` field. Must be `world` or `user`. For GM only modification w/ readability by all users
+          * choose `world`. For unique user private data that can be read / write by a single user choose `user`.
+          */
+         scope: string;
 
          /**
           * When an instance of {@link TJSGameSettings} is defined and automatic game setting registered you may
           * provide default data for the Foundry game setting.
           */
-         defaultData?: WorldArrayObjectStore.Util.ExtractDataType<S>[];
+         defaultData?: GameSettingArrayObject.Util.ExtractDataType<S>[];
 
          /**
-          * An instance of {@link TJSGameSettings}. If provided a world game setting will be automatically registered
-          * for the given `namespace` and `key` with this instance as the associated game setting store.
+          * An instance of {@link TJSGameSettings}. If provided a user game setting will be automatically registered
+          * for the given `namespace`,`key`, and `scope` with this instance as the associated game setting store.
           */
          gameSettings?: TJSGameSettings;
       }
    }
 }
 
-export { WorldArrayObjectStore };
+export { GameSettingArrayObject };
