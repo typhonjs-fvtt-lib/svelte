e3b24d771566f5e43e74f1c8574ec2573ffaa011
typhonrt <support@typhonjs.io>
2025-11-21 20:27:46 -0800

    Update dist.

---
+++ _dist/component/application/data/AppShellOptions.js
@@ -0,0 +1,63 @@
+import { applyVisualEdgeInsets } from '@typhonjs-svelte/runtime-base/svelte/action/dom/style';
+import { isObject }              from '@typhonjs-svelte/runtime-base/util/object';
+
+/**
+ * Sanitation / merging of various app shell props.
+ */
+export class AppShellOptions
+{
+   constructor()
+   {
+      throw new Error('AppShellOptions constructor: This is a static class and should not be constructed.');
+   }
+
+   /**
+    * Sanitizes and potentially merges the `padToVisualEdge` data from `scrollContainer` into `padToVisualEdgeActual`.
+    *
+    * @param {import('@typhonjs-svelte/runtime-base/svelte/action/dom/style').VisualEdgeSides} [padToVisualEdge] - App shell prop.
+    *
+    * @param {boolean | import('@typhonjs-svelte/runtime-base/svelte/component/container').TJSScrollContainerData} [scrollContainer] - App
+    *        shell prop.
+    *
+    * @returns {({
+    *    padToVisualEdgeActual?: import('@typhonjs-svelte/runtime-base/svelte/action/dom/style').VisualEdgeSides,
+    *    scrollContainerActual?: boolean | import('@typhonjs-svelte/runtime-base/svelte/component/container').TJSScrollContainerData
+    * })} Configured padding and scroll container data.
+    */
+   static handlePadScrollOptions(padToVisualEdge, scrollContainer)
+   {
+      // If the app shell prop is valid then use it.
+      let padToVisualEdgeActual = applyVisualEdgeInsets.validateSides(padToVisualEdge) ? padToVisualEdge : void 0;
+
+      let scrollContainerActual = void 0;
+
+      // Potentially copy any `padToVisualEdge` data from `scrollContainer` to `padToVisualEdgeActual` if not already
+      // configured.
+      if (isObject(scrollContainer))
+      {
+         // Shallow copy.
+         scrollContainerActual = Object.assign({}, scrollContainer);
+
+         if (applyVisualEdgeInsets.validateSides(scrollContainerActual.padToVisualEdge))
+         {
+            // Copy `padToVisualEdge` to `padToVisualEdgeActual` if undefined.
+            if (padToVisualEdgeActual === void 0) { padToVisualEdgeActual = scrollContainerActual.padToVisualEdge; }
+
+            // Remove `padToVisualEdge from `scrollContainerActual` data.
+            delete scrollContainerActual.padToVisualEdge;
+         }
+      }
+      else if (typeof scrollContainer === 'boolean' && scrollContainer)
+      {
+         // Convert to object.
+         scrollContainerActual = {};
+      }
+
+      if (padToVisualEdgeActual === void 0)
+      {
+         padToVisualEdgeActual = isObject(scrollContainerActual) ? { right: true } : false;
+      }
+
+      return { padToVisualEdgeActual, scrollContainerActual };
+   }
+}
